e165d0fc83e68a13a8d1f64b2272a6c9b45543bf
TommmyKelly <tommy_kelly@icloud.com>
2021-03-14 12:21:54 +0000

    Converded _app to tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,22 @@
-import '../styles/globals.css'
-import Head from 'next/head'
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
+      <Head>
         <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css'
           integrity='sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=='
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         />
       </Head>
 
-  <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
